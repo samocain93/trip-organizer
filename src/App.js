24f35f4836183042680d92a68522ab40690d2b76
Sam O'Cain <samocain93@gmail.com>
2023-06-13 21:32:44 -0500

    form styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 const initialItems = [
   { id: 1, description: 'Passports', quantity: 2, packed: false },
   { id: 2, description: 'Socks', quantity: 12, packed: false },
-  { id: 3, description: "Phone Charger", quantity: 1, packed: false },
+  { id: 3, description: 'Phone Charger', quantity: 1, packed: false },
 ];
 
 export default function App() {
@@ -21,9 +21,18 @@ function Logo() {
 
 function Form() {
   return (
-    <div className='add-form'>
+    <form className='add-form'>
       <h3>What do you want to pack for your trip?</h3>
-    </div>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Toothbrush..." />
+      <button>Add</button>
+    </form>
   );
 }
 
@@ -42,7 +51,7 @@ function PackingList() {
 function Item({ item }) {
   return (
     <li>
-      <span style={item.packed ? {textDecoration: "line-through" } : {}}>
+      <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
         {item.quantity} {item.description}
       </span>
       <button>❌</button>
