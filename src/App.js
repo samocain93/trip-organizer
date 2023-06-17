9141c7841cab8cb6b7449f8b4505823e2764fdf4
Sam O'Cain <samocain93@gmail.com>
2023-06-17 16:40:16 -0500

    stats calculations and packing items number updated

--- src/App.js
+++ src/App.js
@@ -11,12 +11,24 @@ export default function App() {
     setItems((items) => items.filter((item) => item.id !== id));
   }
 
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
   return (
     <div className='app'>
       <Logo />
       <Form onAddItems={handleAddItems} />
-      <PackingList items={items} onDeleteItem={handleDeleteItem} />
-      <Stats />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+      />
+      <Stats items={items} />
     </div>
   );
 }
@@ -35,7 +47,7 @@ function Form({ onAddItems }) {
     if (!description) return;
 
     const newItem = { description, quantity, packed: false, id: Date.now() };
-    console.log(newItem);
+    // console.log(newItem);
 
     onAddItems(newItem);
 
@@ -61,7 +73,7 @@ function Form({ onAddItems }) {
         placeholder='Toothbrush...'
         value={description}
         onChange={(e) => {
-          console.log(e.target.value);
+          // console.log(e.target.value);
           setDescription(e.target.value);
         }}
       />
@@ -70,21 +82,31 @@ function Form({ onAddItems }) {
   );
 }
 
-function PackingList({ items, onDeleteItem }) {
+function PackingList({ items, onDeleteItem, onToggleItem }) {
   return (
     <div className='list'>
       <ul>
         {items.map((item) => (
-          <Item item={item} key={item.id} onDeleteItem={onDeleteItem} />
+          <Item
+            item={item}
+            key={item.id}
+            onDeleteItem={onDeleteItem}
+            onToggleItem={onToggleItem}
+          />
         ))}
       </ul>
     </div>
   );
 }
 
-function Item({ item, onDeleteItem }) {
+function Item({ item, onDeleteItem, onToggleItem }) {
   return (
     <li>
+      <input
+        type='checkbox'
+        value={item.packed}
+        onChange={() => onToggleItem(item.id)}
+      />
       <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
         {item.quantity} {item.description}
       </span>
@@ -93,10 +115,20 @@ function Item({ item, onDeleteItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length) return <p className='stats'><em>Start adding some items to your packing list 🎒</em></p>
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
   return (
     <footer className='stats'>
-      <em>🧳 You have X items on your list, and you already packed X </em>
+      <em>
+        {percentage === 100
+          ? 'You have everything packed. Ready to go! ✈️'
+          : `🧳 You have ${numItems} items on your list, and you have already packed ${numPacked} items (${percentage}%)`}{' '}
+      </em>
     </footer>
   );
 }
